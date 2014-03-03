db868e728fadfcea8871fd571a94fb46f85144dc
Andreas Hailu <hailuand@umich.edu>
2014-03-03 00:44:43 -0500

    Updated strings/array adapters

--- src/com/example/ahailu/blogreader/MainListActivity.java
+++ src/com/example/ahailu/blogreader/MainListActivity.java
@@ -1,34 +1,27 @@
 package com.example.ahailu.blogreader;
 
 import android.app.ListActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
 
 public class MainListActivity extends ListActivity {
 	
-	protected String[] mAndroidNames = {
-			"Android Beta",
-			"Android 1.0",
-			"Android 1.1",
-			"Cupcake",
-			"Donut",
-			"Eclair",
-			"Froyo",
-			"Gingerbread",
-			"Honeycomb",
-			"Ice Cream Sandwhich",
-			"Jelly Bean"
-	};
+	protected String[] mAndroidNames;
 	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_list);
+		Resources resources = getResources();
+		mAndroidNames= resources.getStringArray(R.array.Android_Names);
 		
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
 		setListAdapter(adapter);
+		
+		//Toast.makeText(this, getString(R.string.no_items), Toast.LENGTH_LONG).show();
 	}
 
 	@Override
