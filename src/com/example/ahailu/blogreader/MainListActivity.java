2dd5333145a070d5af81e497bd1d45302e962a95
Andreas Hailu <hailuand@umich.edu>
2014-03-02 23:09:50 -0500

    Added dummydata, configured adapter

--- src/com/example/ahailu/blogreader/MainListActivity.java
+++ src/com/example/ahailu/blogreader/MainListActivity.java
@@ -3,13 +3,32 @@ package com.example.ahailu.blogreader;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
 public class MainListActivity extends ListActivity {
+	
+	protected String[] mAndroidNames = {
+			"Android Beta",
+			"Android 1.0",
+			"Android 1.1",
+			"Cupcake",
+			"Donut",
+			"Eclair",
+			"Froyo",
+			"Gingerbread",
+			"Honeycomb",
+			"Ice Cream Sandwhich",
+			"Jelly Bean"
+	};
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_list);
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
+		setListAdapter(adapter);
 	}
 
 	@Override
