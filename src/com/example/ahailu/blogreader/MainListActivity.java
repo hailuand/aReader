0f76a21899fd7bc697016b72719c5bdbc5d25a6e
Andreas Hailu <hailuand@umich.edu>
2014-03-04 21:11:18 -0500

    Network connectivity

--- src/com/example/ahailu/blogreader/MainListActivity.java
+++ src/com/example/ahailu/blogreader/MainListActivity.java
@@ -1,25 +1,31 @@
 package com.example.ahailu.blogreader;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.ListActivity;
-import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
-import android.widget.ArrayAdapter;
 
 public class MainListActivity extends ListActivity {
 	
-	protected String[] mAndroidNames;
+	protected String[] mBlogPostTitles;
+	public static final int NUMBER_OF_POSTS = 20;
+	public static final String TAG = MainListActivity.class.getSimpleName();
 	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_list);
-		Resources resources = getResources();
-		mAndroidNames= resources.getStringArray(R.array.Android_Names);
 		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
-		setListAdapter(adapter);
+		
+		getBlogPostsTask GetBlogPostsTask = new getBlogPostsTask();
+		GetBlogPostsTask.execute();
 		
 		//Toast.makeText(this, getString(R.string.no_items), Toast.LENGTH_LONG).show();
 	}
@@ -31,4 +37,30 @@ public class MainListActivity extends ListActivity {
 		return true;
 	}
 
+	private class getBlogPostsTask extends AsyncTask<Object, Void, String> {
+		int responseCode = -1;
+		@Override
+		protected String doInBackground(Object... arg0) {
+			try{
+				URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + 
+			NUMBER_OF_POSTS);
+				HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+				connection.connect();
+				
+				responseCode = connection.getResponseCode();
+				Log.i(TAG, "Code: " + responseCode);
+			}
+			catch(MalformedURLException e){
+				Log.e(TAG, "Exception caught: ", e);
+			}
+			catch(IOException e){
+				Log.e(TAG, "Exception caught: ", e);
+			}
+			catch(Exception e){
+				Log.e(TAG, "Exception caught: ", e);
+			}
+			return "Code :" + responseCode;
+		}
+		
+	}
 }
