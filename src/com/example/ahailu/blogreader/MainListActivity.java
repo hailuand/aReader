3c954d412d465cf6d260a1d267b637df4c55b019
Andreas Hailu <hailuand@umich.edu>
2014-03-05 13:32:52 -0500

    Retrieves JSON data from network

--- src/com/example/ahailu/blogreader/MainListActivity.java
+++ src/com/example/ahailu/blogreader/MainListActivity.java
@@ -1,15 +1,22 @@
 package com.example.ahailu.blogreader;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainListActivity extends ListActivity {
 	
@@ -23,11 +30,33 @@ public class MainListActivity extends ListActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_list);
 		
-		
-		getBlogPostsTask GetBlogPostsTask = new getBlogPostsTask();
-		GetBlogPostsTask.execute();
+		if(isNetworkAvailable()){
+		// Checks network availability
+			getBlogPostsTask GetBlogPostsTask = new getBlogPostsTask();
+			GetBlogPostsTask.execute();
+		}
+		else{
+			//If the network is not available, produced toast message notifying user
+			Toast.makeText(this, "Network is unavailable", Toast.LENGTH_LONG).show();
+		}
 		
 		//Toast.makeText(this, getString(R.string.no_items), Toast.LENGTH_LONG).show();
+	}
+
+	private boolean isNetworkAvailable() {
+		ConnectivityManager manager = 
+				(ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		// The ConnectivityManager class answers queries about the state of the network connectivity
+		NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+		// The NetworkInfo class describes the status of a network interface
+		// The getActiveNetworkInfo() method returns details about the currently active data network. Returns a
+		// NetworkInfo object.
+		
+		boolean isAvailable = false; 
+		if(networkInfo != null && networkInfo.isConnected()) {
+			isAvailable = true;
+		}
+		return isAvailable;
 	}
 
 	@Override
@@ -38,6 +67,9 @@ public class MainListActivity extends ListActivity {
 	}
 
 	private class getBlogPostsTask extends AsyncTask<Object, Void, String> {
+	// Connects to URL and retrieves data from it
+	// An AsyncTasks does work in parallel with the main UI thread, so that the UI remains responsive to user
+	// interface actions, and doesn't stop everything to do one task
 		int responseCode = -1;
 		@Override
 		protected String doInBackground(Object... arg0) {
@@ -48,6 +80,21 @@ public class MainListActivity extends ListActivity {
 				connection.connect();
 				
 				responseCode = connection.getResponseCode();
+				if(responseCode == HttpURLConnection.HTTP_OK){
+					InputStream inputStream = connection.getInputStream();
+					// An InputStream is data stored as bytes.
+					Reader reader = new InputStreamReader(inputStream);
+					// Reader reads the data from the InputStream as we know that we are dealing with strings
+					int contentLength = connection.getContentLength(); 
+					// Connection knows how many chars we're dealing with
+					char[] charArray = new char[contentLength];
+					reader.read(charArray); // Store data in our char array. The read method changes the data in char
+					String responseData = new String(charArray); // Convert charArray to strings
+					Log.v(TAG, responseData); // Write this data to the log (JSON)
+				}
+				else{
+					Log.i(TAG, "Unsuccessful HTTP Response Code: " + responseCode);
+				}
 				Log.i(TAG, "Code: " + responseCode);
 			}
 			catch(MalformedURLException e){
